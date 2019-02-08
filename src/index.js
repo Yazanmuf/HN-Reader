7d5ef4c0d4c9ba635f8c95fe29928a8a28a4f791
Mohamed M <mo@districtm.ca>
2019-02-07 22:08:58 -0500

    hot reload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello yaz");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'HN Reader';
+
+ReactDOM.render(
+<div>{title}</div>,
+document.getElementById('app')
+);
+
+module.hot.accept();
