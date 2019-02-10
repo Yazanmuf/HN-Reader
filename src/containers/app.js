13d905aa08b299ed581dd079c4c941442759729c
Yazan Mufleh <ymufleh@gmail.com>
2019-02-09 22:26:40 -0500

    created local state, and list constant within the container

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
+const list = [
+    {
+        title: 'React',
+        author: 'Jordan Walke',
+    }
+]
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            list: list,
+        };
+    }
+
+
     render() {
-        return <h1> Hello </h1>
+        return (
+            <div className="App">
+                {this.state.list.map(item =>
+                    <div>
+                        <span>{item.title}</span>
+                    </div>
+                )}
+
+            </div>
+        )
     }
 }
+
 
 export default App;
