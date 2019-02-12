ff4bfaaeb5c887bde34d60bda0ad2f2ecf0a70cf
Yazan Mufleh <ymufleh@gmail.com>
2019-02-12 14:33:11 -0500

    almost there, I can't access my returned object from fetch even though I can see it in the console?

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,47 +1,67 @@
 import React, { Component } from 'react';
 
 
+
 class App extends Component {
     constructor(props) {
         super(props);
 
-        const storyId = 19143064
+        // const storyId = 19143064
 
         this.state = {
-            id: [],
-            title: [],
+            // id: [],
+            // title: [],
             storyList: [],
         };
     }
+
     componentDidMount() {
         fetch('https://hacker-news.firebaseio.com/v0/jobstories.json?print=pretty')
             .then(response => response.json())
             .then(stories => {
                 this.setState({ storyList: stories });
             });
-        const storyId = 19143064
+        // const storyId = 19143064;
 
-        fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`)
-            .then(response => response.json())
-            .then(data => {
-                this.setState({
-                    id: storyId,
-                    title: data.title,
-                });
-            })
+
+        // fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`)
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         this.setState({
+        //             id: storyId,
+        //             title: data.title,
+        //         });
+        //     })
 
     }
 
     render() {
-        const { id, title } = this.state;
+        const { storyList } = this.state;
+        // console.log(storyList)
         return (
-            <div className="App">
-                <span>{id}</span><br />
-                <span>{title}</span><br />
+            <div>
+                <ul>
+                    {
+                        storyList.map(
+                            (id) => {
+                                fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+                                    .then(response => {
+                                        console.log(response)
+                                        response.json()
+                                    })
+                                    .then(data => {
+                                        console.log('Why is this step undefined?=====>', data)
+                                        return (
+                                            <li>{data}</li>
+                                        );
+                                    });
+                            })
+
+                    }
+                </ul>
             </div>
         )
-    }
+    };
+
 }
-
-
 export default App;
