f9186f48148aa23241bafe0ba8d76ed71c30eb4c
Yazan Mufleh <ymufleh@gmail.com>
2019-02-15 15:00:08 -0500

    unable to update state with multiple api calls. tried several methods. console.logs are for me to track progress as app updates

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,65 +1,50 @@
 import React, { Component } from 'react';
 
-
-
 class App extends Component {
     constructor(props) {
         super(props);
 
-        // const storyId = 19143064
-
         this.state = {
-            // id: [],
-            // title: [],
             storyList: [],
+            storyObjects: [],
         };
     }
 
     componentDidMount() {
+        const { storyList } = this.state;
         fetch('https://hacker-news.firebaseio.com/v0/jobstories.json?print=pretty')
             .then(response => response.json())
             .then(stories => {
                 this.setState({ storyList: stories });
             });
-        // const storyId = 19143064;
+        console.log('componentDidMount phase complete', storyList)
 
 
-        // fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`)
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         this.setState({
-        //             id: storyId,
-        //             title: data.title,
-        //         });
-        //     })
 
     }
 
     render() {
-        const { storyList } = this.state;
-        // console.log(storyList)
-        return (
-            <div>
-                <ul>
-                    {
-                        storyList.map(
-                            (id) => {
-                                fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
-                                    .then(response => {
-                                        console.log(response)
-                                        response.json()
-                                    })
-                                    .then(data => {
-                                        console.log('Why is this step undefined?=====>', data)
-                                        return (
-                                            <li>{data}</li>
-                                        );
-                                    });
-                            })
+        const newStoryObjectList = []
+        this.state.storyList.slice(0, 10).map(
+            (id, n) => {
+                fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+                    .then(response => {
+                        response.json();
+                        // I can see the returned promise when I console log the response here. Why is it undefined in the next step?
+                    })
+                    .then(data => {
+                        newStoryObjectList[n] = data;
+                        console.log('Data goes here', id, data)
+                    })
+                    ;
+            })
 
-                    }
+        return (
+            <div >
+                <ul>
+                    {/* {storyObjects} */}
                 </ul>
-            </div>
+            </div >
         )
     };
 
