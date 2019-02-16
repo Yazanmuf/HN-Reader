5f0fac22a0d9460fe745d65678e6752454aa8de1
Mohamed M <mo@districtm.ca>
2019-02-15 21:52:44 -0500

    api calls complete

--- src/api.js
+++ src/api.js
@@ -4,3 +4,7 @@ const ENDPOINT = 'https://hacker-news.firebaseio.com/v0/';
 const fetchTopStoryIds = (limit = 10) => fetch(ENDPOINT + 'topstories.json')
     .then(r => r.json())
     .then(ids => ids.slice(0, limit));
+
+const fetchStory = id => fetch(ENDPOINT + `item/${id}.json`).then(r => r.json());
+
+const fetchStories = ids => Promise.all(ids.map(id => fetchStory(id)));
