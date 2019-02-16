fa3ac94b07e0660df4a3c1564c0a56ca26ae6713
Mohamed M <mo@districtm.ca>
2019-02-15 21:51:54 -0500

    fetcher for top stories

--- src/api.js
+++ src/api.js
@@ -1,1 +1,6 @@
 const ENDPOINT = 'https://hacker-news.firebaseio.com/v0/';
+
+
+const fetchTopStoryIds = (limit = 10) => fetch(ENDPOINT + 'topstories.json')
+    .then(r => r.json())
+    .then(ids => ids.slice(0, limit));
